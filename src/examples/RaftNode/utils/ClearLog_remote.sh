bd96e3d0d137fb1bd7e8be378a0454fbdeeaab45
Anton Werenberg <anton.werenberg@copenhagenatomics.com>
2021-12-08 15:02:58 +0100

    clean up of utility script

--- src/examples/RaftNode/utils/ClearLog_remote.sh
+++ src/examples/RaftNode/utils/ClearLog_remote.sh
@@ -2,9 +2,6 @@ HOSTS=$1
 echo "Deleting /home/pi/testC/"
 for host in $(cat $HOSTS); do ssh $host "rm /home/pi/testC/ -r"; done
 
-echo "Deleting /home/pi/storage/"
-for host in $(cat $HOSTS); do ssh $host "rm /home/pi/storage/ -r"; done
-
 echo "Deleting /home/pi/run_remote.sh"
 for host in $(cat $HOSTS); do ssh $host "rm /home/pi/run_remote.sh -r"; done
 
